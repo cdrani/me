a6f52b286399b928a502b4c6b3b2ab80175c68ab
cdrani <cdrani@users.noreply.github.com>
2019-02-19 23:00:01 -0700

    remove unnecessary icons

--- src/utils/get-icon.js
+++ src/utils/get-icon.js
@@ -10,12 +10,6 @@ const getIcon = name => {
     case 'github':
       icon = ICONS.GITHUB
       break
-    case 'vkontakte':
-      icon = ICONS.VKONTAKTE
-      break
-    case 'telegram':
-      icon = ICONS.TELEGRAM
-      break
     case 'email':
       icon = ICONS.EMAIL
       break
@@ -24,9 +18,6 @@ const getIcon = name => {
       break
     case 'resume':
       icon = ICONS.RESUME
-      break
-    case 'rss':
-      icon = ICONS.RSS
       break
     default:
       icon = {}
