856d88bd85a8232bd4b9377e4efb97773a0be275
cdrani <charlesdrani@gmail.com>
2019-05-31 16:18:00 -0600

    add twitter card

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import styles from './Layout.module.scss';
+import React from 'react'
+import Helmet from 'react-helmet'
+import styles from './Layout.module.scss'
 
 const Layout = ({ children, title, description }) => (
   <div className={styles.layout}>
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta property="og:site_name" content={title} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
     </Helmet>
     {children}
   </div>
-);
+)
 
-export default Layout;
+export default Layout
